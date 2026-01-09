2fcbc09d10dbb58141c0352f68c67b18422e3910
WarPigC <callaniruddhji@gmail.com>
2026-01-10 02:58:26 +0530

    theme change (fix ashen)

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,30 @@
 return {
 
-{
-    "ficcdaf/ashen.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme ashen]])
-    end,
-  }
+	{
+		"ficcdaf/ashen.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		tag = "*",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		opts = {
+			colorscheme = "ashen",
+		}
+	},
 
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {  }
+	},
+
+	{
+		"rose-pine/neovim",
+		lazy = false,
+		priority = 10000,
+		config = function()
+			vim.cmd[[
+			colorscheme rose-pine
+			]]
+		end
+	}
 }
