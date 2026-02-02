06f05cc14be5a9bd05c4b263df48930b5c8e4346
WarPigC <callaniruddhji@gmail.com>
2026-02-02 20:48:50 +0530

    ashen minor change

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,11 +3,7 @@ return {
 	{
 		"ficcdaf/ashen.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		tag = "*",
 		priority = 1000, -- make sure to load this before all the other start plugins
-		opts = {
-			colorscheme = "ashen",
-		}
 	},
 
 	{
